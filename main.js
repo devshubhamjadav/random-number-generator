94117d068c3ef3aa43a5ffaf1e24fae3c67e9dc2
unknown <shubhamverma6844@gmail.com>
2023-03-30 17:49:28 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const getColor = () => {
+    const randomNumber = Math.floor(Math.random() * 16777215);
+    const randomcode = "#" + randomNumber.toString(16);
+    document.body.style.backgroundColor = randomcode;
+    document.getElementById("color-code").innerText = randomcode;
+}
+
+document.getElementById("click-btn").addEventListener("click", getColor)
+
+getColor();
